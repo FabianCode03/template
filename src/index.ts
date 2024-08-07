2919aa3aa4e5ea9c9fd0ec196b5f007b74f11bdb
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-07 22:49:55 +0200

    Add example module

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import "./style.css";
+import exampleModule from "./modules/exampleModule";
 
 const testTitle = "Template working!";
+const testModule = exampleModule("Hello, world!");
 
 const titleElement = document.createElement("h1");
 titleElement.textContent = testTitle;
 titleElement.classList.add("title");
 
+const moduleElement = document.createElement("p");
+moduleElement.textContent = testModule;
+moduleElement.classList.add("module");
+
 document.body.appendChild(titleElement);
+document.body.appendChild(moduleElement);
 
 console.log("Hello, world!");
---
+++ src/modules/exampleModule.ts
@@ -0,0 +1,5 @@
+// standard webpack exampleModule.ts
+
+export default function exampleModule(someArg: string): string {
+  return `This is an example module with arg: ${someArg}`;
+}
