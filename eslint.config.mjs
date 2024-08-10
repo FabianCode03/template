44f33cc5a525b8934e2e57f6c602864c9bbca7a4
Fabian_PC <fb-224651@hs-weingarten.de>
2024-08-10 20:02:39 +0200

    Setup config files for Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import { rules } from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      ...rules,
+      'no-undef': 'off',
+    },
+  },
 );
